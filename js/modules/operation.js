3d887c48a09b2ac5c98d05833ecf1a8f244d3d54
Henrique Oliveira <wandholiveira1@gmail.com>
2025-02-18 14:59:19 -0300

    :black_bird: Added 'close' class.

--- js/modules/operation.js
+++ js/modules/operation.js
@@ -1,16 +1,16 @@
 export default function operation () {
+  const openingHours = document.querySelector('[data-week]');
+  const weekDays = openingHours.dataset.week.split(',').map(Number);
+  const weekHours = openingHours.dataset.hours.split(',').map(Number);
+  const dateNow = new Date();
+  const today = dateNow.getDay();
+  const hourNow = dateNow.getHours();
+  const openDay = weekDays.indexOf(today) !== -1;
+  const openHour = (hourNow >= weekHours[0] && hourNow < weekHours[1]);
   
-}
-
-const openingHours = document.querySelector('[data-week]');
-const weekDays = openingHours.dataset.week.split(',').map(Number);
-const weekHours = openingHours.dataset.hours.split(',').map(Number);
-const dateNow = new Date();
-const today = dateNow.getDay();
-const hourNow = dateNow.getHours();
-const openDay = weekDays.indexOf(today) !== -1;
-const openHour = (hourNow >= weekHours[0] && hourNow < weekHours[1]);
-
-if(openDay && openHour) {
-  openingHours.classList.add('open');
+  if(openDay && openHour) {
+    openingHours.classList.add('open');
+  }else{
+    openingHours.classList.add('close');
+  } 
 }
